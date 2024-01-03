c6ddaaec94719f5d8534bc79ddf6734ba784498c
Andrew Zolensky <andrewlzolensky@gmail.com>
2024-01-03 12:09:30 -0600

    Demo'd primitive data types

--- JavaGitDemo/src/App.java
+++ JavaGitDemo/src/App.java
@@ -5,13 +5,37 @@ import java.util.Scanner;
 public class App {
 
 	public static void main(String[] args) {
-		// program to take input in fahrenheit and convert to celcius
-		double f, c;
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter the temperature in Fahrenheit:");
-		f = scan.nextDouble();
-		c = (5.0/9.0)*(f-32);
+		// boolean (1 bit)
+		boolean bl = false;
+		System.out.println(bl);
+		
+		// byte (8 bits)
+		byte bt = 0x0A;
+		System.out.println(bt);
+		
+		// char (16 bits)
+		char c = 'a';
 		System.out.println(c);
+		
+		// short (16 bits)
+		short sh = 100;
+		System.out.println(sh);
+		
+		// int (32 bits)
+		int i = 7000;
+		System.out.println(i);
+		
+		// long (64 bits)
+		long l = 200000;
+		System.out.println(l);
+		
+		// float (32 bits)
+		float fl = 1.0756f;
+		System.out.println(fl);
+				
+		
+		// double (64 bits)
+		double db = 1.46837620604363;
+		System.out.println(db);
 	}
-
 }
