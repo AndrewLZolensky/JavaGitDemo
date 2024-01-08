41fd8491a5936c3db8864b30a89d763bb4bf6e1f
Andrew Zolensky <andrewlzolensky@gmail.com>
2024-01-07 20:27:19 -0600

    Experimented with classes

--- JavaGitDemo/src/App.java
+++ JavaGitDemo/src/App.java
@@ -5,37 +5,43 @@ import java.util.Scanner;
 public class App {
 
 	public static void main(String[] args) {
-		// boolean (1 bit)
-		boolean bl = false;
-		System.out.println(bl);
+		Person pearson = new Person(22, "pearson");
+		Person parson = new Person(23, "parson");
+		Person piercesSon = new Person(27, "Pierce's Son");
+		Person[] persons = {pearson, parson, piercesSon};
+		People people = new People(persons);
+		people.printPeople();
 		
-		// byte (8 bits)
-		byte bt = 0x0A;
-		System.out.println(bt);
-		
-		// char (16 bits)
-		char c = 'a';
-		System.out.println(c);
-		
-		// short (16 bits)
-		short sh = 100;
-		System.out.println(sh);
-		
-		// int (32 bits)
-		int i = 7000;
-		System.out.println(i);
-		
-		// long (64 bits)
-		long l = 200000;
-		System.out.println(l);
-		
-		// float (32 bits)
-		float fl = 1.0756f;
-		System.out.println(fl);
-				
-		
-		// double (64 bits)
-		double db = 1.46837620604363;
-		System.out.println(db);
 	}
 }
+
+class Person {
+	int age;
+	String name;
+	
+	Person(int age, String name) {
+		this.age = age;
+		this.name = name;
+	}
+	
+	void printPerson() {
+		System.out.println("Person named " + this.name + " is " + this.age + " years old.");
+	}
+}
+
+class People {
+	Person[] people;
+	
+	People(Person[] people) {
+		this.people = people;
+	}
+	
+	void printPeople() {
+		for (Person p : people) {
+			p.printPerson();
+		}
+	}
+}
+
+
+
