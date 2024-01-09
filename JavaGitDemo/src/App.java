ef7e9214cfecbaa1067dd1b8d9a0a800de52a1e5
Andrew Zolensky <andrewlzolensky@gmail.com>
2024-01-09 00:30:44 -0500

    Added a new package.

--- JavaGitDemo/src/App.java
+++ JavaGitDemo/src/App.java
@@ -1,27 +1,16 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println(Thing.numberOfThings);
-		Thing thing1 = new Thing(22);
-		System.out.println(thing1.age);
-		System.out.println(Thing.numberOfThings);
-		Thing thing2 = new Thing(24);
-		System.out.println(thing2.age);
-		System.out.println(Thing.numberOfThings);
 		
-	}
-}
-
-class Thing {
-	public static int numberOfThings;
-	public int age;
-	
-	Thing(int age) {
-		this.age = age;
-		numberOfThings++;
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		arr.add(12);
+		arr.add(4);
+		System.out.println(arr.toString());
+		System.out.println(arr.get(1));
 	}
 }
